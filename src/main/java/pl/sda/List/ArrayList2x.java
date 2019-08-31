1ca31c920f47042a9a4b515ca7df8d268530101e
Lukasz Maslaczyk <lukasz.maslaczyk@gmail.com>
2019-08-31 12:45:38 +0200

    long list implementation

--- src/main/java/pl/sda/List/ArrayList2x.java
+++ src/main/java/pl/sda/List/ArrayList2x.java
@@ -1,45 +1,41 @@
 package pl.sda.List;
-
 import java.util.Arrays;
-
 public class ArrayList2x implements IList {
-
-    private final long[] holder;
+    private long[] holder;
     private int size = 0;
     private final int arraySizeMultipler = 2;
-
     public ArrayList2x() {
         holder = new long[arraySizeMultipler];
     }
-
     // to do
     @Override
     public boolean isEmpty() {
-        return false;
+        return size ==0 ? true :false;
     }
-
     @Override
     public int size() {
         return size;
     }
-
     @Override
     public long get(int index) {
-        return 0;
+        checkBounds(index);
+        return holder[index];
     }
-
-    //to do
     @Override
     public void set(int index, long value) {
-
+        checkBounds(index);
+        holder[index] = value;
     }
-
     //to do
     @Override
     public void remove(int index) {
+        checkBounds(index);
+        for (int i = index; i < size; i++) {
+            holder[i] = holder[i + 1];
+        }
+        size --;
 
     }
-
     @Override
     public int firstIndexWith(long value) {
         for (int i = 0; i < size; i++) {
@@ -49,29 +45,55 @@ public class ArrayList2x implements IList {
         }
         return -1;
     }
-
     //to do - increment size of table
     @Override            //   0             2           size= 1
     public void add(int index, long value) {
+        checkInsertBounds(index);
+        if (holder.length ==size)
+            incermentHolderLenght();
         for (int i = size; i > index; i--) {
             holder[i] = holder[i - 1];
         }
         holder[index] = value;
         size++;
     }
-
+    private void incermentHolderLenght() {
+        int newHolderSize = holder.length + arraySizeMultipler;
+        long[] newHolder = new long[newHolderSize];
+        for (int i= 0; i < size; i++)
+            newHolder[i] = holder[i];
+        holder = newHolder;
+    }
     //to do - increment size of physical table
+
     @Override
     public void add(long value) {
+        if (holder.length ==size)
+            incermentHolderLenght();
         holder[size] = value;
         size++;
     }
-
     @Override
     //TODO - This should return copy of the filled part of the array
     public long[] getHolderView() {
         return Arrays.copyOfRange(holder, 0, size);
-
     }
 
+    private void checkBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+    }
+    //when inserting value you can add it between other or exactly at the end which is index = size
+    private void checkInsertBounds(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+    }
 }
+
+
+
+
+
+
